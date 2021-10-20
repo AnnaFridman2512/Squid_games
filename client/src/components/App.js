3a866c64e3dd227a76e43535782f13ea27b2ac60
levi <levihalperin1998@gmail.com>
2021-10-20 15:22:28 +0300

    started working on nps

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,30 +1,30 @@
 import './App.css';
 import React from "react";
-import {BrowserRouter as Router,Switch, Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Nav from './Nav.js';
 import HomePage from './HomePage.js';
 import ChoosePlayer from './ChoosePlayer.js';
-import Game from './Game.js';
+import Game from './Game/Game.js';
 
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Nav /> 
-          <Switch>
-            <Route path="/" exact>
-              <HomePage />
-            </Route>
-            <Route path="/choosePlayer">
-              <ChoosePlayer />
-            </Route>
-            <Route path="/game">
-              <Game />
-            </Route>
-          </Switch>
+        <Nav />
+        <Switch>
+          <Route path="/" exact>
+            <HomePage />
+          </Route>
+          <Route path="/choosePlayer">
+            <ChoosePlayer />
+          </Route>
+          <Route path="/game">
+            <Game />
+          </Route>
+        </Switch>
       </Router>
-    </div>  
+    </div>
   );
 }
 
--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,23 +1,23 @@
 import './Nav.css';
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Nav() {
-    
-    return (
-      <nav>
-        <div className="logo">
-          <Link to='/'>
-            <div className="homepage-link"></div>
-            </Link>
-        </div>
-        <div className="links">
-          <Link to="choosePlayer">
-            Choose player
-          </Link>
-          <Link to="Canvas">
-            Play
-          </Link>
-        </div>
-      </nav>
-    );
-  }
+
+  return (
+    <nav>
+      <div className="logo">
+        <Link to='/'>
+          <div className="homepage-link"></div>
+        </Link>
+      </div>
+      <div className="links">
+        <Link to="choosePlayer">
+          Choose player
+        </Link>
+        <Link to="Game">
+          Play
+        </Link>
+      </div>
+    </nav>
+  );
+}
