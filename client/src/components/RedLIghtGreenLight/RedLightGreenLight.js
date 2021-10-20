3fcb31dbadb4fd3fe9c6eb8e9e95c753b39edb96
levi <levihalperin1998@gmail.com>
2021-10-20 15:44:32 +0300

    started working on nps

---
+++ client/src/components/RedLIghtGreenLight/RedLightGreenLight.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useRef } from 'react'
+import data from './data';
+import { Npcs } from './Npcs';
+
+export default function Game() {
+    const canvasRef = useRef(null);
+    useEffect(() => {
+
+        const canvas = canvasRef.current
+        const ctx = canvas.getContext('2d')
+        // canvas styling
+        canvas.width = window.innerWidth
+        canvas.height = window.innerHeight - 160
+        canvas.style.backgroundColor = 'beige'
+        // starting to draw
+        let npc = Npcs(ctx, data.npcObj)
+        const render = () => {
+            ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+            npc.movement()
+
+
+            requestAnimationFrame(render)
+        }
+        render()
+
+
+    }, [])
+    return <div>
+        <h1>Welcome to the Red Light Green Light</h1>
+        <canvas ref={canvasRef}></canvas>
+    </div>
+}
