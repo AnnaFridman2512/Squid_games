c9aa68c7f31d88a6ec78997f66fc5e885292195f
menachemlev12@gmail.com <menachemlev12@gmail.com>
2021-11-18 19:30:53 +0200

    update

--- client/src/components/Doll/Doll.js
+++ client/src/components/Doll/Doll.js
@@ -9,7 +9,7 @@ function Model({ ...props }) {
   return (
     <group ref={group} {...props} dispose={null}>
       <group rotation={[-Math.PI, 0, 0]}>
-        <group rotation={[Math.PI / 2, 0, 0]} scale={0.01}>
+        <group rotation={[Math.PI / 2, Math.PI / 3, 0]} scale={0.01}>
           <group rotation={[-Math.PI / 2, 0, 0]} scale={[100, 100, 100]}>
             <mesh
               geometry={
