3b9603c6633962526a75e2f28839aad748b0268d
Anna-Fridman <annafridman81@gmail.com>
2021-11-21 20:21:24 +0200

    deployment

--- server/server.mjs
+++ server/server.mjs
@@ -15,12 +15,13 @@ app.use(express.urlencoded()); //Parse URL-encoded bodies
 app.use(cors());
 
 app.use("/api/players", express.static(path.join(__dirname, "players"))); //adding path to use at client side to fetch pics
-
-app.use(express.static('../client/build'));
-
 app.use("/api/photos", express.static(path.join(__dirname, "photos"))); //adding path to use at client side to fetch pics
 
+
 app.use("/api/players", playersRouter);
+app.use(express.static('../client/build'));
+
+
 
 const port = process.env.PORT || 8080; //listening to heroku port Or 8080 default
 
