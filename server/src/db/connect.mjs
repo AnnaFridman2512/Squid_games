b0f28210fc6f00faaf4444dc0fc53ffcb5f3e040
Anna-Fridman <annafridman81@gmail.com>
2021-11-23 11:48:19 +0200

    dbURL apdated

--- server/src/db/connect.mjs
+++ server/src/db/connect.mjs
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const dbURL = process.env.DB_URL_Y || process.env.DB_URL || "mongodb://localhost:27017/squid_game";
+const dbURL = process.env.DB_URL || "mongodb://localhost:27017/squid_game";
 
 
 export const connect = () => {
