c306ce70e5463266d26558b5e4cb6da6b27163a2
Anna-Fridman <annafridman81@gmail.com>
2021-11-29 11:22:31 +0200

    code cleanUp+ changes were made to make npc movement more random

--- client/src/components/Game/Npc.js
+++ client/src/components/Game/Npc.js
@@ -1,70 +1,64 @@
 import "./Game.css";
-import { animate, motion, useAnimation } from "framer-motion";
+import {  motion, useAnimation } from "framer-motion";
 import { useEffect, useRef, useState } from "react";
 
 export default function Npc({
   index,
   number,
-  key,
   gameIsOn,
   greenLight,
   reportNpcBoundries,
   checkHide,
   killNpc,
-  npcsAmount,
   checkForCoveringNpcs,
-  resetPosition,
-  image,
+  //resetPosition,
+  //image,
 }) {
   const controls = useAnimation();
   const npcRef = useRef(null);
-  let xRand = Math.floor(
-    window.innerWidth * ((npcsAmount - index) / npcsAmount)
-  );
-  let yTand = Math.ceil(-window.screen.height);
   const [playing, setPlaying] = useState(true);
+  let npcWidth = 40;
+  let npcHeigth = 35;
+  let maxWidth = window.innerWidth - npcWidth;
+  let maxHeight = window.innerHeight - npcHeigth; 
+  let newRight = Math.floor(Math.random() * maxWidth);
+  let newLeft = Math.floor(Math.random() *-maxWidth);
+  let newTop = Math.floor(Math.random() * -maxHeight*2);
 
-  useEffect(() => {
-    setInterval(() => {
-      reportNpcBoundries(npcRef?.current?.getBoundingClientRect(), index);
-    }, 100);
-  }, []);
-
-  useEffect(() => {}, [resetPosition]); //HELP
 
   useEffect(() => {
     checkHide(npcRef?.current?.getBoundingClientRect());
-  }, [checkForCoveringNpcs]);
+  }, [checkForCoveringNpcs, checkHide]);
 
   useEffect(() => {
-    if (!gameIsOn || !playing) return;
-    if (greenLight) {
-      controls.start((i) => ({
-        x: xRand,
-        y: yTand,
-        transition: {
-          duration: 20,
-        },
-      }));
-    }
-  }, [gameIsOn, greenLight]);
+    if (!playing || !gameIsOn){
+      return;
+    } else if(greenLight){
+        let npcX = setInterval(() => {
+          reportNpcBoundries(npcRef?.current?.getBoundingClientRect(), index);
+        }, 100);
 
-  useEffect(() => {
-    if (!gameIsOn) {
-      controls.stop();
-    }
-  }, [gameIsOn]);
+        if(npcX * 60 < maxWidth){
+          controls.start(() => ({
+            x: newRight,
+            y: newTop,
+            transition: {duration: 20}
+          }));  
+        }else{
+          controls.start(() => ({
+           x: newLeft,
+           y: newTop,
+           transition: {duration: 20}
+        }));  
+       }
 
-  useEffect(() => {
-    if (!gameIsOn || !playing) return;
-
-    if (!greenLight) {
+    }else{//If !greenLight stop random npcs
       if (Math.random() > 0.25) {
         controls.stop();
       } else {
-        controls.start((i) => ({
-          x: xRand,
-          y: yTand,
+        controls.start(() => ({
+          x: newRight,
+          y: newTop,
           transition: {
             duration: 15,
           },
@@ -76,14 +70,13 @@ export default function Npc({
         }, 1000);
       }
     }
-  }, [greenLight, gameIsOn]);
 
-  const calcaluteLeft = () => {};
+  }, [checkForCoveringNpcs, gameIsOn, greenLight, playing]);
 
   return (
     <motion.div
       ref={npcRef}
-      className="playerInGame"
+      className="npc"
       animate={controls}
       style={{
         left: `${index * 14}%`,
