64ba6be79fd74ba578a9680f259bae26d3458fbf
Anna-Fridman <annafridman81@gmail.com>
2021-12-04 14:45:54 +0200

    jump to game when player chosen- added

--- client/src/components/Player/Player.js
+++ client/src/components/Player/Player.js
@@ -1,7 +1,7 @@
 import "./Player.css";
 import { useContext } from "react";
 import { PlayersContext } from "../Players/PlayersContext.js";
-
+import { Link } from "react-router-dom";
 export default function Player({ _id, number, image }) {
   const { choosePlayer } = useContext(PlayersContext);
 
@@ -16,7 +16,9 @@ export default function Player({ _id, number, image }) {
         <p className="player-number">Player {number}</p>
         {
           <button onClick={() => choosePlayer(number)}>
+            <Link className="span-link" to="game">
             <span>Choose</span>
+            </Link>
           </button>
         }
       </div>
